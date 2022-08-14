9965577965299fe03121f1adec91f464f86a22a5
Jyothi <bjyothipriya.999@gmail.com>
2022-08-14 22:52:51 +0200

    aravind explanation

--- script/form.js
+++ script/form.js
@@ -1,118 +1,61 @@
-
-//$(document).ready(function(){
-//click on button submit
-//  $("#submit").on('click',
-//    function () {
-
-let form_data = new FormData();
-$.ajax({
-    type: "POST",
-    url: "file.json",
-    dataType: 'json',
-    data: $("#form").serialize(),
-    success: function (response) {
-        console.log(response)
-    }
-
+//Global variables
+let x = [];
+//this code will execute after page loading
+$( document ).ready(function() {
+    let y = {};
+    //take the file from the file.json
+    takeFileFromJson();
 });
 
+function showDataForTask(fileData){
+    console.log("This is data from the file.json",fileData);
 
-let object = {
+    /*
+    console.log("this is the type of fileData ---",typeof(fileData));
+    console.log("this is the type of fileData ---",typeof(x));
+    console.log("this is the type of fileData ---",typeof(y));
+    */
 
+    $.each(fileData, function (key, value) {
+        console.log(key);
+        console.log(value);
+    });
+}
 
-    contact: {
-        standard: [
-            {
-                field_name: "email",
-                "marketing-in-one": "email",
-                email: "",
-                datatype: "Email",
-                required: 1,
-                html_label: "Email",
-                html_placeholder: "Email",
-                html_hidden_field: 0,
-                field_type: "Standard",
-                php_type: "string",
-                regex: "^[_a-zA-Z0-9-]+(.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(.[a-zA-Z0-9-]+)*(.[a-z]{2,9})$"
-            },
-            {
-                field_name: "last_name",
-                "marketing-in-one": "last_name",
-                last_name: "",
-                datatype: "Text",
-                required: 0,
-                html_label: "Text",
-                html_placeholder: "LastName",
-                html_hidden_field: 0,
-                field_type: "Standard",
-                php_type: "string",
-                regex: ".*"
-            },
-            {
-                field_name: "last_name",
-                "marketing-in-one": "last_name",
-                last_name: "",
-                datatype: "Text",
-                required: 0,
-                html_label: "Text",
-                html_placeholder: "LastName",
-                html_hidden_field: 0,
-                field_type: "Standard",
-                php_type: "string",
-                regex: ".*"
-            },
-            {
-                field_name: "salutation",
-                "marketing-in-one": "salutation",
-                salutation: "",
-                datatype: "Text",
-                required: 0,
-                html_label: "Text",
-                html_placeholder: "Salutation",
-                html_hidden_field: 0,
-                field_type: "Standard",
-                php_type: "string",
-                regex: ".*"
-            },
-            {
-                field_name: "street",
-                "marketing-in-one": "street",
-                street: "",
-                datatype: "Text",
-                required: 0,
-                html_label: "Text",
-                html_placeholder: "Street",
-                html_hidden_field: 0,
-                field_type: "Standard",
-                php_type: "string",
-                regex: ".*"
-            },
-            {
-                field_name: "phone",
-                "marketing-in-one": "phone",
-                phone: "",
-                datatype: "Phone",
-                required: 0,
-                html_label: "Phone",
-                html_placeholder: "Phone",
-                html_hidden_field: 1,
-                field_type: "Standard",
-                php_type: "string",
-                regex: "[0-9-/ ()+]*"
-            }
-        ]
-    }
+function takeFileFromJson() {
+    $.ajax({
+        type: "GET",
+        async:"false",
+        url: "data/file.json",
+        success: function (response) {
+            showDataForTask(response);
+        }
+    });
+}
+
+function learningFunction(a,b,c,d){
+    //vadukovatam vachu anatadaniki example
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
 }
 
 
 
-console.log(Object.keys(object))
-console.log(Object.keys(object.contact.standard[0]))
-console.log(Object.values(object.contact.standard[1]))
-console.log(Object.values(object.contact.standard[2]))
-console.log(Object.values(object.contact.standard[3]))
-console.log(Object.values(object.contact.standard[4]))
-console.log(Object.values(object.contact.standard[5]))
+
+
+
+
+
+
+
+//console.log(Object.keys(object.contact.standard[0]))
+//console.log(Object.values(object.contact.standard[1]))
+//console.log(Object.values(object.contact.standard[2]))
+//console.log(Object.values(object.contact.standard[3]))
+//console.log(Object.values(object.contact.standard[4]))
+//console.log(Object.values(object.contact.standard[5]))
 
 
 // for (let key of Object.keys(object.contact.standard[0])) {
